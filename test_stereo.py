36e1c060e716e05176fb90681dc0fc29e71bb9f9
lelimite4444 <lelimite4444@gmail.com>
2019-05-14 19:50:20 +0800

    Update train and test file

--- test_stereo.py
+++ test_stereo.py
@@ -23,6 +23,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     
     parser.add_argument('--model_name',                type=str,   help='model name', default='monodepth')
+    parser.add_argument('--split',                     type=str,   help='validation set', default='kitti')
     parser.add_argument('--data_path',                 type=str,   help='path to the data', required=True)
     parser.add_argument('--filenames_file',            type=str,   help='path to the filenames text file', required=True)
     parser.add_argument('--input_height',              type=int,   help='input height', default=256)
@@ -41,40 +42,30 @@ elif args.model_name == 'pwc':
     args.input_width = 832
 net.load_state_dict(checkpoint['state_dict'])
 
-former_test, latter_test, flow = get_flow_data(args.filenames_file, args.data_path)
-TestFlowLoader = torch.utils.data.DataLoader(
-        myImageFolder(former_test, latter_test, flow, args),
-        batch_size = 1, shuffle = False, num_workers = 1, drop_last = False)
+left_image_test, right_image_test = get_data(args.filenames_file, args.data_path)
+TestImageLoader = torch.utils.data.DataLoader(
+         myImageFolder(left_image_test, right_image_test, None, args),
+         batch_size = 1, shuffle = False, num_workers = 1, drop_last =False)
 
-total_error = 0
-fl_error = 0
-num_test = 0
-for batch_idx, (left, right, gt, mask, h, w) in enumerate(TestFlowLoader, 0):
-    
+if args.split == 'kitti':
+    disparities = np.zeros((200, args.input_height, args.input_width), dtype=np.float32)
+elif args.split == 'eigen':
+    disparities = np.zeros((697, args.input_height, args.input_width), dtype=np.float32)
+
+for batch_idx, (left, right) in enumerate(TestImageLoader, 0):
+    print(batch_idx)
     left_batch = torch.cat((left, torch.from_numpy(np.flip(left.numpy(), 3).copy())), 0)
     right_batch = torch.cat((right, torch.from_numpy(np.flip(right.numpy(), 3).copy())), 0)
     
-    left = Variable(left_batch.cuda())
-    right = Variable(right_batch.cuda())
-
-    model_input = torch.cat((left, right), 1)
+    left_pyramid = make_pyramid(left_batch, 4)
+    
+    model_input = Variable(torch.cat((left_batch, right_batch), 1).cuda())
     if args.model_name == 'monodepth':
-        disp_est_scale, disp_est = net(model_input)
+        disp_est_scale, disp_est= net(model_input)
     elif args.model_name == 'pwc':
         disp_est_scale = net(model_input)
-
-    mask = np.ceil(np.clip(np.abs(gt[0,0]), 0, 1))
-
-    disp_ori_scale = nn.UpsamplingBilinear2d(size=(int(h), int(w)))(disp_est_scale[0][:1])
-    disp_ori_scale[0,0] = disp_ori_scale[0,0] * int(w) / args.input_width
-    disp_ori_scale[0,1] = disp_ori_scale[0,1] * int(h) / args.input_height
-
-    error, fl = evaluate_flow(disp_ori_scale[0].data.cpu().numpy(), gt[0].numpy(), mask.numpy())
-    total_error += error
-    fl_error += fl
-    num_test += 1
+        disp_est = [torch.cat((disp_est_scale[i][:,0,:,:].unsqueeze(1) / disp_est_scale[i].shape[3], disp_est_scale[i][:,1,:,:].unsqueeze(1) / disp_est_scale[i].shape[2]), 1) for i in range(4)]
     
-total_error /= num_test
-fl_error /= num_test
-print("The average EPE is : ", total_error)
-print("The average Fl is : ", fl_error)
+    disparities[batch_idx] = -disp_est[0][0,0,:,:].data.cpu().numpy()
+print('done')
+np.save('./disparities.npy', disparities)
